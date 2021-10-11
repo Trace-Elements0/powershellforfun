8342762e2370e88a947cbdbf140d6e621554d9dd
Julian Edington <arcanicvoid@gmail.com>
2021-10-11 15:33:52 -0500

    Adm elevate new session, shortened env var strings
    - Replaced old function with a proper admin elevation section.
    - Using a variant of string interpolation rather than concatenation.
    - Single quote vs double quote distinctions; clarifying variables inside.

--- install-dependencies.ps1
+++ install-dependencies.ps1
@@ -1,7 +1,35 @@
-############
-# GO ADMIN #
-############
-function GoAdmin { Start-Process pwsh –Verb RunAs }
+##########################################################################
+# THIS SCRIPT WILL INSTALL A PROFILE ALONG WITH DEPENDENCIES NEEDED RUNS #
+# WELL WITH POWERSHELL CORE 7.X; WINDOWS POWERSHELL COULD HAVE ISSUES    #
+##########################################################################
+
+
+####################################################
+# IF NOT IN A SESSION AS ADMIN, RELOADS A SEPARATE # 
+# INSTANCE + CONTINUES SCRIPT WITHIN THAT SESSION  #
+####################################################
+param([switch]$Elevated)
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $(
+        [Security.Principal.WindowsIdentity]::GetCurrent()
+    )
+    $currentUser.IsInRole(
+        [Security.Principal.WindowsBuiltinRole]::Administrator
+    )
+}
+if (!(Test-Admin))  {
+    if ($Elevated) {
+        # tried to elevate, did not work, aborting
+    } 
+    else {
+        Start-Process pwsh.exe -Verb RunAs -ArgumentList (
+            '-noprofile -noexit -file "{0}" -elevated' -f (
+                $myinvocation.MyCommand.Definition
+            )
+        )
+    }
+    exit
+}
 
 ######################################
 # INSTALL MODULES IF THEY DONT EXIST #
@@ -10,7 +38,7 @@ If(-not(Get-InstalledModule posh-git -ErrorAction silentlycontinue)){
 	Install-Module posh-git -Scope CurrentUser -Confirm:$False -Force
 }
 If(-not(Get-InstalledModule oh-my-posh -ErrorAction silentlycontinue)){
-	Install-Module oh-my-posh -Scope CurrentUser -RequiredVersion 2.0.412 -Confirm:$False -Force
+	Install-Module oh-my-posh -Scope CurrentUser -Confirm:$False -Force
 }
 If(-not(Get-InstalledModule PSReadline -AllowPrerelease -ErrorAction silentlycontinue)){
 	Install-Module PSReadline -Scope CurrentUser -AllowPrerelease -Confirm:$False -Force
@@ -29,25 +57,25 @@ If(!(Test-Path $profile)){
 ###################################################
 # CREATE BLANK C:\TEMP FOLDER IF ONE DOESNT EXIST #
 ###################################################
-If(!(Test-Path $($env:HOMEDRIVE + '\Temp'))){
-	New-Item -ItemType Directory -Path $env:HOMEDRIVE -Name "Temp"
+If(!(Test-Path "$env:HOMEDRIVE\Temp")){
+	New-Item -ItemType Directory -Path $env:HOMEDRIVE -Name 'Temp'
 }
 
 ############################
 # ADD NEW TEMP DIR TO PATH #
 ############################
-#Get the users current path value
-$CurrentValue = [Environment]::GetEnvironmentVariable("Path", "User")    
-#Add the new path, careful when using this second part manually
+# Get the users current path value
+$CurrentValue = [Environment]::GetEnvironmentVariable('Path', 'User')    
+# Add the new path, careful when using this second part manually
 If($CurrentValue){
-	[Environment]::SetEnvironmentVariable("Path", $CurrentValue + [System.IO.Path]::PathSeparator + "$env:HOMEDRIVE\Temp", "User")
+	[Environment]::SetEnvironmentVariable('Path', $CurrentValue + [System.IO.Path]::PathSeparator + "$env:HOMEDRIVE\Temp", 'User')
 }
 
 ##################################
 # WEB-SCRAPE GITHUB SCRIPT FILES #
 ##################################
-Invoke-WebRequest -Uri "raw.githubusercontent.com/Trace-Elements0/powershellforfun/main/profile.ps1" | Select-Object -ExpandProperty Content | Out-File $profile -Encoding unicode -Force
-Invoke-WebRequest -Uri "raw.githubusercontent.com/Trace-Elements0/powershellforfun/main/Show-Object.ps1" -OutFile $($env:HOMEDRIVE + '\Temp\Show-Object.ps1')
-Invoke-WebRequest -Uri "raw.githubusercontent.com/Trace-Elements0/powershellforfun/main/Select-TextOutput.ps1" -OutFile $($env:HOMEDRIVE + '\Temp\Select-TextOutput.ps1')
-Invoke-WebRequest -Uri "raw.githubusercontent.com/Trace-Elements0/powershellforfun/main/Get-ParameterAlias.ps1" -OutFile $($env:HOMEDRIVE + '\Temp\Get-ParameterAlias.ps1')
-Invoke-WebRequest -Uri "raw.githubusercontent.com/Trace-Elements0/powershellforfun/main/Get-AliasSuggestion.ps1" -OutFile $($env:HOMEDRIVE + '\Temp\Get-AliasSuggestion.ps1')
+Invoke-WebRequest -Uri 'raw.githubusercontent.com/Trace-Elements0/powershellforfun/main/profile.ps1' | Select-Object -ExpandProperty Content | Out-File $profile -Encoding unicode -Force
+Invoke-WebRequest -Uri 'raw.githubusercontent.com/Trace-Elements0/powershellforfun/main/Show-Object.ps1' -OutFile "$env:HOMEDRIVE\Temp\Show-Object.ps1"
+Invoke-WebRequest -Uri 'raw.githubusercontent.com/Trace-Elements0/powershellforfun/main/Select-TextOutput.ps1' -OutFile "$env:HOMEDRIVE\Temp\Select-TextOutput.ps1"
+Invoke-WebRequest -Uri 'raw.githubusercontent.com/Trace-Elements0/powershellforfun/main/Get-ParameterAlias.ps1' -OutFile "$env:HOMEDRIVE\Temp\Get-ParameterAlias.ps1"
+Invoke-WebRequest -Uri 'raw.githubusercontent.com/Trace-Elements0/powershellforfun/main/Get-AliasSuggestion.ps1' -OutFile "$env:HOMEDRIVE\Temp\Get-AliasSuggestion.ps1"
